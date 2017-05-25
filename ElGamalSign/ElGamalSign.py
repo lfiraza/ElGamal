c7a2f0803b44ee2e4c7110572ce72cfdc11d26b8
Lukasz Firaza <lukasz.firaza@gmail.com>
2017-05-25 19:16:45 +0200

    described necessery operations

--- ElGamalSign/ElGamalSign.py
+++ ElGamalSign/ElGamalSign.py
@@ -4,5 +4,15 @@
 # p = prime
 # g < p && x < p 
 
-y = pow(g, x, p)
+#y = pow(g, x, p)
+
+# k random
+# gdc(k, p-1) = 1
+
+# a = pow(g, k, p)
+# M = (xa + kb) mod (p-1) - egdc searching b
+
+# signature - a and b
+
+# checker - (y^a * a^b) mod p = g^M mod p
 
